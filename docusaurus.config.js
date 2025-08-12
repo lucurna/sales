79bd454081570599b438193b12683bfdbad77ff5
lucurna <lucurna@gmail.com>
2025-08-12 08:09:52 -0500

    add card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,7 +77,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/lucurna-social-card.png',
       navbar: {
         title: 'LucUrna',
         logo: {
